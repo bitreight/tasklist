914bc9498df2e9a2c1024a0772b229859f8c09d3
bitreight <alexey.kur@gmail.com>
2016-10-21 11:04:13 +0300

    HomeController returns home.jsp.

--- tasklist/src/main/java/com/bitreight/tasklist/controller/HomeController.java
+++ tasklist/src/main/java/com/bitreight/tasklist/controller/HomeController.java
@@ -2,14 +2,12 @@ package com.bitreight.tasklist.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    @ResponseBody
     public String goHome() {
-        return "Welcome home!";
+        return "home";
     }
 }
