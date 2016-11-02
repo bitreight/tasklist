8707b2e5e48f1320044272809d40fb08a64c72fa
bitreight <alexey.kur@gmail.com>
2016-11-03 02:29:31 +0300

    Configured h2 test database.

---
+++ tasklist-backend/src/test/resources/tasklist.sql
@@ -0,0 +1,51 @@
+-- -----------------------------------------------------
+-- Table `users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(20) NOT NULL,
+  `password` VARCHAR(20) NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `surname` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `projects`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `projects` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(20) NOT NULL,
+  `description` VARCHAR(500) NULL,
+  `users_owner_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `projects_title_UNIQUE` (`title` ASC),
+  CONSTRAINT `fk_projects_users1`
+    FOREIGN KEY (`users_owner_id`)
+    REFERENCES `users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tasks`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(500) NULL,
+  `deadline` DATE NULL,
+  `task_priority` VARCHAR(10) NOT NULL,
+  `projects_id` INT NOT NULL,
+  `is_completed` TINYINT(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `tasks_title_UNIQUE` (`title` ASC),
+  CONSTRAINT `fk_tasks_projects`
+    FOREIGN KEY (`projects_id`)
+    REFERENCES `projects` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
