9e7552f8d406ebfd6dc005655b34df651addaa97
bitreight <alexey.kur@gmail.com>
2016-12-03 01:03:20 +0300

    Added exception handlers.

--- tasklist/src/main/java/com/bitreight/tasklist/handler/ApiError.java
+++ tasklist/src/main/java/com/bitreight/tasklist/handler/ApiError.java
@@ -2,15 +2,27 @@ package com.bitreight.tasklist.handler;
 
 import org.springframework.http.HttpStatus;
 
+import java.util.Collections;
+import java.util.List;
+
 public class ApiError {
 
     private String message;
 
     private HttpStatus status;
 
+    private List<String> errors;
+
     public ApiError(String message, HttpStatus status) {
         this.message = message;
         this.status = status;
+        this.errors = Collections.singletonList(message);
+    }
+
+    public ApiError(String message, HttpStatus status, List<String> errors) {
+        this.message = message;
+        this.status = status;
+        this.errors = errors;
     }
 
     public String getMessage() {
@@ -19,5 +31,9 @@ public class ApiError {
 
     public HttpStatus getStatus() {
         return status;
+    }
+
+    public List<String> getErrors() {
+        return errors;
     }
 }
--- tasklist/src/main/java/com/bitreight/tasklist/handler/CustomRestExceptionHandler.java
+++ tasklist/src/main/java/com/bitreight/tasklist/handler/CustomRestExceptionHandler.java
@@ -5,11 +5,20 @@ import com.bitreight.tasklist.service.exception.ServiceProjectAlreadyExistsExcep
 import com.bitreight.tasklist.service.exception.ServiceProjectNotFoundException;
 import com.bitreight.tasklist.service.exception.ServiceTaskAlreadyExistsException;
 import com.bitreight.tasklist.service.exception.ServiceTaskNotFoundException;
+import com.bitreight.tasklist.service.exception.ServiceTaskVersionIsOutdatedException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -20,15 +29,39 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 
-    @ExceptionHandler({ServiceProjectAlreadyExistsException.class, ServiceTaskAlreadyExistsException.class})
-    public ResponseEntity<Object> handleAlreadyExists(ServiceException e) {
+    @ExceptionHandler({ServiceProjectAlreadyExistsException.class, ServiceTaskAlreadyExistsException.class,
+            ServiceTaskVersionIsOutdatedException.class})
+    public ResponseEntity<Object> handleConflict(ServiceException e) {
         ApiError apiError = new ApiError(e.getLocalizedMessage(), HttpStatus.CONFLICT);
         return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler({IllegalArgumentException.class})
-    public ResponseEntity<Object> handleInvalidParameters(RuntimeException e) {
+    public ResponseEntity<Object> handleInvalidRequestParameters(RuntimeException e) {
         ApiError apiError = new ApiError(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleAnyException(Exception e) {
+        ApiError apiError = new ApiError("Error happened while processing the request.",
+                HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                               HttpHeaders headers, HttpStatus status,
+                                                               WebRequest request) {
+        List<String> errors = new ArrayList<String>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + ": " + error.getDefaultMessage());
+        }
+        for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+            errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+        }
+
+        ApiError apiError = new ApiError(ex.getLocalizedMessage(), HttpStatus.BAD_REQUEST, errors);
+        return handleExceptionInternal(ex, apiError, headers, apiError.getStatus(), request);
     }
 }
