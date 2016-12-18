534fcd1e65e1e92caf6aba42b0d9c20ba1f28d0d
bitreight <alexey.kur@gmail.com>
2016-12-19 00:29:39 +0300

    Secured task services.

--- tasklist-backend/src/main/java/com/bitreight/tasklist/service/TaskSearchService.java
+++ tasklist-backend/src/main/java/com/bitreight/tasklist/service/TaskSearchService.java
@@ -4,20 +4,25 @@ import com.bitreight.tasklist.dto.TaskDto;
 import com.bitreight.tasklist.service.exception.ServiceProjectNotFoundException;
 import com.bitreight.tasklist.service.exception.ServiceTaskNotFoundException;
 import com.bitreight.tasklist.service.exception.ServiceUserNotFoundException;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface TaskSearchService {
 
+    @PreAuthorize("principal.id == #userId")
     List<TaskDto> getByUserId(int userId, String primarySortKey) throws ServiceUserNotFoundException, ServiceTaskNotFoundException;
 
+    @PreAuthorize("@securityService.isProjectOwner(#projectId)")
     List<TaskDto> getByProjectId(int projectId, String primarySortKey)
             throws ServiceProjectNotFoundException, ServiceTaskNotFoundException;
 
+    @PreAuthorize("principal.id == #userId")
     List<TaskDto> getTodayTasksByUserId(int userId, LocalDate clientCurrentDate, String primarySortKey)
             throws ServiceUserNotFoundException, ServiceTaskNotFoundException;
 
+    @PreAuthorize("principal.id == #userId")
     List<TaskDto> getWeekTasksByUserId(int userId, LocalDate clientCurrentDate, String primarySortKey)
             throws ServiceUserNotFoundException, ServiceTaskNotFoundException;
 }
